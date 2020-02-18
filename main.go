5bad98244f2275932da87c5d1d8364efc6df1b44
Miguel Erja <gomi@xxxlgroup.com>
2020-02-18 13:53:15 +0100

    extract CSV input logic

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"os"
 )
 
-func main() {
-	file, error := os.Open("problems.csv")
+func getCSVFile() *os.File {
+	inputReader := bufio.NewReader(os.Stdin)
+	text, _ := inputReader.ReadString('\n')
+	var fileSrc string
+	if len(os.Args) > 1 {
+		fileSrc = os.Args[1]
+	} else {
+		fileSrc = "problems.csv"
+	}
+
+	file, error := os.Open(fileSrc)
+	fmt.Print(error)
 	if error != nil {
 		fmt.Print(error)
 	}
+	return file
+}
 
-	reader := csv.NewReader(file)
+func main() {
+	file := getCSVFile()
+	csvReader := csv.NewReader(file)
 
 	for {
-		record, err := reader.Read()
+		record, err := csvReader.Read()
 		if err == io.EOF {
 			break
 		}
