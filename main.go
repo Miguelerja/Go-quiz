dba5b8b6cd0b7c0e8f516fd32341045bd1ef93f1
Miguel Erja <gomi@xxxlgroup.com>
2020-02-21 07:29:52 +0100

    refactor scanner and add first logic for questions

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 	"os"
 )
 
-// setScanner creates a new scanner, asks the user for input and returns the input value
-func setScanner() string {
-	var csvFile string
+// setScanner creates a new scanner
+func setScanner() *bufio.Scanner {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	return scanner
+}
+
+// getUserInitialConfig gets user inputs to configure the game's time and csv to be used
+func getUserInitialConfig() string {
+	var csvFile string
+	scanner := setScanner()
 
 	fmt.Print("Enter your custom CSV file. If no file is entered, the default file will be used:\n")
 	scanner.Scan()
@@ -25,7 +32,7 @@ func getCSVFile() *os.File {
 	var fileSrc string
 	defaultCsvFile := "problems.csv"
 
-	csvFile := setScanner()
+	csvFile := getUserInitialConfig()
 
 	if len(csvFile) > 0 {
 		fileSrc = csvFile
@@ -41,25 +48,36 @@ func getCSVFile() *os.File {
 	return file
 }
 
-func parseCSV(file *os.File) {
-	csvReader := csv.NewReader(file)
+func parseCSV(csvFile *os.File) [][]string {
+	csvReader := csv.NewReader(csvFile)
+	var results [][]string
 
 	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
+		record, error := csvReader.Read()
+
+		if error == io.EOF {
 			break
 		}
-		if err != nil {
-			fmt.Print(err)
+		if error != nil {
+			fmt.Print(error)
+			break
 		}
 
-		fmt.Print(record)
+		results = append(results, record)
 	}
 
+	return results
 }
 
 func main() {
+	// points := 0
 	file := getCSVFile()
+	questions := parseCSV(file)
 
-	parseCSV(file)
+	for i := 0; i < len(questions); i++ {
+		question := questions[i][0]
+		answer := questions[i][1]
+		fmt.Print(question)
+		fmt.Print(answer)
+	}
 }
